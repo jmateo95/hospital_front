675bc6f3cedf58f0d278578c62a5704e510a959a
jmateo95 <201313852@ingenieria.usac.edu.gt>
2022-02-22 19:50:15 -0600

    Admin

---
+++ src/app/Administrator/dash-admin/dash-admin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashAdminRoutingModule } from './dash-admin-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DashAdminRoutingModule
+  ]
+})
+export class DashAdminModule { }
