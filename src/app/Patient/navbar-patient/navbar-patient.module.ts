0e86bf05372abc7267d6a9b8734b573ac9628639
Fatima Odra Daniela Tezo Sum <fatimatezo201831039@cunoc.edu.gt>
2022-02-22 21:14:24 -0600

    ADD: NavBar Patient

---
+++ src/app/Patient/navbar-patient/navbar-patient.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavBarRoutingModule } from './navbar-patient-routing.module';
+import { MaterialModule } from '../../material/material.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    NavBarRoutingModule,
+    
+  ]
+})
+export class NavbarPatientModule { }
