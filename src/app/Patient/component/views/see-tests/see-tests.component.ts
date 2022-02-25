332ee5aab87f4581d3764c840ffe39d4b2c44c14
Fatima Odra Daniela Tezo Sum <fatimatezo201831039@cunoc.edu.gt>
2022-02-25 02:12:59 -0600

    ADD: Cards-test,appointment,calendar;View:see-test

---
+++ src/app/Patient/component/views/see-tests/see-tests.component.ts
@@ -0,0 +1,71 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { delay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-see-tests',
+  templateUrl: './see-tests.component.html',
+  styleUrls: ['./see-tests.component.css']
+})
+export class SeeTestsComponent implements OnInit {
+
+  breakpoint = 3;
+  cards = [
+    { title: 'Title 1', content: 'Content 1' },
+    { title: 'Title 2', content: 'Content 2' },
+    { title: 'Title 3', content: 'Content 3' },
+    { title: 'Title 4', content: 'Content 4' }
+  ];
+  constructor(private observer: BreakpointObserver) { }
+
+
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.observer
+      .observe(['(min-width: 1200px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.breakpoint = 3;
+        }
+      });
+    this.observer
+      .observe(['(max-width: 1200px) and (min-width: 925px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.breakpoint = 2;
+        }
+      });
+
+    this.observer
+      .observe(['(max-width: 925px) and (min-width: 800px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.breakpoint = 1;
+        }
+      });
+    this.observer
+      .observe(['(max-width: 800px) and (min-width: 625px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.breakpoint = 2;
+        }
+      });
+    this.observer
+      .observe(['(max-width: 625px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.breakpoint = 1;
+        }
+      });
+
+  }
+
+}
