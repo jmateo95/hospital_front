5ebe9d125a0295cf289d6d947ce8c2253cc6d071
Fatima Odra Daniela Tezo Sum <fatimatezo201831039@cunoc.edu.gt>
2022-02-26 20:45:35 -0600

    FIX: Home patient view

--- src/app/Patient/home-patient/home-patient.component.ts
+++ src/app/Patient/home-patient/home-patient.component.ts
@@ -1,15 +1,67 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
-
+import { delay } from 'rxjs/operators';
 @Component({
   selector: 'app-home-patient',
   templateUrl: './home-patient.component.html',
   styleUrls: ['./home-patient.component.css']
 })
 export class HomePatientComponent implements OnInit {
-
-  constructor() { }
+  breakpoint = 4;
+  hidepicture = false;
+  constructor(private observer: BreakpointObserver) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.observer
+      .observe(['(min-width: 1200px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.breakpoint = 4;
+          this.hidepicture = false;
+        }
+      });
+    this.observer
+      .observe(['(max-width: 1200px) and (min-width: 925px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.breakpoint = 2;
+          this.hidepicture = false;
+        }
+      });
+
+    this.observer
+      .observe(['(max-width: 925px) and (min-width: 800px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.breakpoint = 1;
+          this.hidepicture = true;
+        }
+      });
+    this.observer
+      .observe(['(max-width: 800px) and (min-width: 625px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.breakpoint = 2;
+          this.hidepicture = false;
+        }
+      });
+    this.observer
+      .observe(['(max-width: 625px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.breakpoint = 1;
+          this.hidepicture = true;
+        }
+      });
+
   }
 
 }
