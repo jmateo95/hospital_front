332773b8060fb800461d90ce062af9be9f335a5a
jmateo95 <201313852@ingenieria.usac.edu.gt>
2022-03-24 06:47:16 -0600

    rutas admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import {  VigilanteGuard } from 'src/app/vigilante.guard';
 
 const routes: Routes = [
   {path:'', redirectTo:'login', pathMatch:'full'},
   {path:'login', component:LoginComponent},
   {path:'register', component:RegisterComponent},
-  {path: 'administrator', loadChildren: () => import('./Administrator/dash-admin/dash-admin.module').then(x => x.DashAdminModule) },
+  {
+    path: 'administrator',
+    loadChildren: () => import('./Administrator/dash-admin/dash-admin.module').then(x => x.DashAdminModule),
+    canActivateChild:[VigilanteGuard]
+  },
   {path:'doctor', loadChildren: () => import('./Doctor/DoctorTopbar/doctorTopbar.module').then(x => x.DoctorTopbarModule)},
   {path:'lab', loadChildren: () => import('./Lab/LapTopbar/LabTopbar.module').then(x => x.LabTopbarModule)},
   {path: 'patient', loadChildren: () => import('./Patient/navbar-patient/navbar-patient.module').then(x => x.NavbarPatientModule) },
